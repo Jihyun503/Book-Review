2ec5cd08192ebafdbb6e5f775b457b034eb2704d
Jihyun503 <s2017s17@e-mirim.hs.kr>
2022-08-17 23:18:46 +0900

    언어 및 시간대 설정

--- bookreview/settings.py
+++ bookreview/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-0kg7li7_%4835@w(@c5j8^*8j(ji9j&jo%_gj&*8=e&=7_)3yr"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -99,9 +99,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ko-kr"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Seoul"
 
 USE_I18N = True
 
