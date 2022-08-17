711a779ec6bebc9600842543f66e53133084688d
Jihyun503 <s2017s17@e-mirim.hs.kr>
2022-08-17 23:59:59 +0900

    로그아웃 구현

--- common/views.py
+++ common/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import render, redirect
 
-from reviews.models import User
+from .models import User
 from .forms import *
 
 # Create your views here.
+def signup(request):
+    signup_form = SignupForm()
+    context = {'forms' : signup_form}
+
+    if request.method == 'GET':
+        return render(request, "signup.html", context)
+    
+    elif request.method == 'POST':
+        signup_form = SignupForm(request.POST)
+        if signup_form.is_valid():
+            user = User(
+                id = signup_form.id,
+                pwd = signup_form.pwd,
+                name = signup_form.name,
+                email = signup_form.email,
+                nickname = signup_form.nickname
+            )
+            user.save()
+            return redirect('/')
+        else:
+            context['forms'] = signup_form
+            if signup_form.errors:
+                for value in signup_form.errors.values():
+                    context['error'] = value
+        return render(request, "signup.html", context)
+
 def login(request):
     login_form = LoginForm()
     context = {'forms' : login_form}
@@ -27,3 +53,7 @@ def login(request):
             '''
             context['forms'] = login_form
         return render(request, "login.html", context)
+
+def logout(request):
+    request.session.flush()
+    return redirect('/')
