32c1fcc73e9ed368abd82ff39197629142804553
Jihyun503 <s2017s17@e-mirim.hs.kr>
2022-08-22 03:57:55 +0900

    리뷰 수정 및 삭제 기능 구현

--- reviews/views.py
+++ reviews/views.py
@@ -72,4 +72,65 @@ def reviewDetail(request, pk):
 
     context['board'] = board
     context['scope'] = scope_form
+
+    if board.writer.id == login_session:
+        context['writer'] = True
+    else:
+        context['writer'] = False
+
     return render(request, "review_detail.html", context)
+
+
+def reviewModify(request, pk):
+    login_session = request.session.get("user", "")
+
+    context = {}
+    context['login_session'] = login_session
+
+    board = get_object_or_404(Board, bno=pk)
+    context['board'] = board
+
+    if board.writer.id != login_session:
+        return redirect(f'/review/detail/{pk}/')
+
+    if request.method == 'GET':
+        review_form = ReviewWriteForm(instance=board)
+        context['forms'] = review_form
+        return render(request, "review_modify.html", context)
+
+    elif request.method == 'POST':
+        review_form = ReviewWriteForm(request.POST)
+
+        if review_form.is_valid():
+            login_session = request.session.get('user', '')
+            writer = User.objects.get(id=login_session)
+            meta_json = {'scope': review_form.meta_json}
+
+            board.title = review_form.title
+            board.contents = review_form.contents
+            board.meta_json = str(meta_json)
+            
+            board.save()
+            return redirect('/review')
+        else:
+            '''
+            context['forms'] = join_form
+            if join_form.errors:
+                for value in join_form.errors.values():
+                    context['error'] = value
+            '''
+            context['forms'] = review_form
+        return render(request, "review_modify.html", context)
+
+
+def reviewDelete(request, pk):
+    login_session = request.session.get("user", "")
+
+    board = get_object_or_404(Board, bno=pk)
+    
+    if board.writer.id == login_session:
+        board.delete()
+        return redirect('/review')
+    else:
+        return redirect(f'/review/detail/{pk}/')
+
