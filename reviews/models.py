7e70111650a2432a0cb118e4e0bbf0410e9699a6
Jihyun503 <s2017s17@e-mirim.hs.kr>
2022-08-22 03:56:05 +0900

    수정시간 옵션 수정

--- reviews/models.py
+++ reviews/models.py
@@ -12,6 +12,6 @@ class Board(models.Model):
     writer = models.ForeignKey('common.User', on_delete=models.CASCADE)
     category = models.CharField(max_length=32, db_index=True)
     write_date = models.DateTimeField(auto_now_add=True)
-    update_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
     hits = models.PositiveIntegerField(default=0)
     meta_json = models.TextField() # 별점이나 추가적으로 다른 데이터가 들어갈 때
