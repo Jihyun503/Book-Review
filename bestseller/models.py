fbbee05b3060c9c9d8bd28bbf7ae6852ad0f12e4
Jihyun503 <s2017s17@e-mirim.hs.kr>
2022-08-23 04:18:05 +0900

    베스트셀러 모델 생성

---
+++ bestseller/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Bestseller(models.Model):
+    title = models.CharField(max_length=64)
+    author = models.CharField(max_length=32)
+    price = models.IntegerField()
+    pub_date = models.CharField(max_length=10)
+    bookcover = models.TextField()
+    standard_week = models.CharField(max_length=6)
