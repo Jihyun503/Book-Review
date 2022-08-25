afc8d0ebde5ea9a30e346434d41ee9bf8ed7e8b0
Jihyun503 <s2017s17@e-mirim.hs.kr>
2022-08-26 02:30:35 +0900

    독서 일정 리스트 구현

--- schedule/views.py
+++ schedule/views.py
@@ -1,5 +1,6 @@
+from asyncio import futures
 from django.shortcuts import render, redirect, get_object_or_404
-
+from datetime import timedelta, datetime
 from .models import Schedule
 from common.models import User
 from .forms import *
@@ -11,11 +12,26 @@ from common.decorators import login_required
 def scheduleList(request, **kwargs):
     context = {}
     context['login_session'] = kwargs.get("login_session")
+    login_session = request.session.get('user', '')
 
     # 일단 모든 일정을 가져온 뒤에 목표 날짜가 오늘 날짜보다 이후면 진행중, 목표날짜가 오늘보다 이전날짜면 완료된 일정
-    schedule = Schedule.objects.all()
+    schedules = Schedule.objects.all()
+    now = []
+    past = []
+    future = []
 
-    context['schedules'] = schedule
+    for schedule in schedules:
+        if schedule.start_date > datetime.today() and schedule.end_date > datetime.today():
+            future.append(schedule)
+        elif schedule.start_date <= datetime.today() and schedule.end_date >= datetime.today():
+            now.append(schedule)
+        elif schedule.end_date < datetime.today():
+            past.append(schedule)
+
+    context['now'] = now
+    context['past'] = past
+    context['future'] = future
+    context['user'] = login_session
     
     return render(request, "schedule_list.html", context)
 
